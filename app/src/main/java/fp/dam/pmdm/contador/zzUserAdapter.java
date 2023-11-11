5327d4c2178e94cd6de8becd82287485c61fdf0c
Águeda Prieto Llamas <aguedapl55@educastur.es>
2023-11-11 12:52:55 +0100

    preCambio ListView-RecyclerView

--- app/src/main/java/fp/dam/pmdm/contador/zzUserAdapter.java
+++ app/src/main/java/fp/dam/pmdm/contador/zzUserAdapter.java
@@ -21,7 +21,7 @@ public class zzUserAdapter extends RecyclerView.Adapter<zzUserAdapter.ViewHolder
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.zz_fila, parent, false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.zz_fila_recyclerview, parent, false);
         return new ViewHolder(v);
     }
 
@@ -40,7 +40,7 @@ public class zzUserAdapter extends RecyclerView.Adapter<zzUserAdapter.ViewHolder
 
         public ViewHolder(View v) {
             super (v);
-            nombre = (TextView) v.findViewById(R.id.filaNombre);
+            nombre = (TextView) v.findViewById(R.id.filaTexto);
         }
     }
 }
